7944d76edce58a66804149ca2e3ee840d1b13055
OleksandrUntervald <untervaldthebest@gmail.com>
2025-10-18 19:22:08 +0300

    Lesson7Сontext API-memo

--- src/Components/LeftBranch.tsx
+++ src/Components/LeftBranch.tsx
@@ -1,7 +1,15 @@
 import LeftBranchA from "./LeftBranchA.tsx";
+import {useMemo} from "react";
 
 
 const LeftBranch = () => {
+   const memox = useMemo(() => {
+
+        for (let i = 0; i < 10; i++) {
+            console.log(i)
+        }
+        return null
+    }, [])
     return (
         <div>
             LeftBranch
